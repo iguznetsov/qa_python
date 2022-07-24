f06f534a75d7f772c29d795913138cb07f75e31b
IgorKuznetsov <igor.ku1505@yandex.ru>
2022-07-24 16:49:46 +0300

    before pull

--- tests.py
+++ tests.py
@@ -26,68 +26,68 @@ class TestBooksCollector:
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
 
-    def test_set_book_rating_0(self):  # рейтинг меньше 1 присвоить нельзя
+    def test_set_book_rating_0(self):
         collector = BooksCollector()
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.set_book_rating('Гордость и предубеждение и зомби', 0)
         assert collector.get_book_rating('Гордость и предубеждение и зомби') != 0
 
-    def test_set_book_rating_12(self):  # рейтинг больше 11 присвоить нельзя
+    def test_set_book_rating_12(self):
         collector = BooksCollector()
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.set_book_rating('Гордость и предубеждение и зомби', 12)
         assert collector.get_book_rating('Гордость и предубеждение и зомби') != 12
 
-    def test_set_book_raiting_9(self):  # можно присваивать рейтинг меньше 10 и больше 1
+    def test_set_book_raiting_9(self):
         collector = BooksCollector()
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.set_book_rating('Гордость и предубеждение и зомби', 9)
         assert collector.get_book_rating('Гордость и предубеждение и зомби') == 9
 
-    def test_set_book_rating_book_not_in_list(self):  # нельзя присвоить рейтинг книги , которая не в списке книиг
+    def test_set_book_rating_book_not_in_list(self):
         collector = BooksCollector()
         collector.set_book_rating('Гордость и предубеждение и зомби', 2)
         assert len(collector.books_rating) == 0
 
-    def test_get_book_rating_by_name(self):  # получаем рейтинг книги по ее имени
+    def test_get_book_rating_by_name(self):
         collector = BooksCollector()
         collector.add_new_book('Гордость и предубеждение и зомби')
         assert collector.get_book_rating('Гордость и предубеждение и зомби') == 1
 
-    def test_get_book_rating_withour_rating(self):  # нет рейтинга у книги , которая еще не добавлена
+    def test_get_book_rating_withour_rating(self):
         collector = BooksCollector()
         assert collector.get_book_rating('Гордость и предубеждение и зомби') == None
 
-    def test_get_books_with_specific_rating_1(self):  # выводим список книг с определенным рейтингом
+    def test_get_books_with_specific_rating_1(self):
         collector = BooksCollector()
         collector.add_new_book('Гордость и предубеждение и зомби')
         assert collector.get_books_with_specific_rating(1) == ['Гордость и предубеждение и зомби']
 
-    def test_add_book_in_favorites_books(self): # проверка добавления книги в избранное
+    def test_add_book_in_favorites_books(self):
         collector = BooksCollector()
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_book_in_favorites('Гордость и предубеждение и зомби')
         assert 'Гордость и предубеждение и зомби' in collector.get_list_of_favorites_books()
 
-    def test_delete_book_from_favorites_books(self): # проверяем удаления книги из ищбранных
+    def test_delete_book_from_favorites_books(self):
         collector = BooksCollector()
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_book_in_favorites('Гордость и предубеждение и зомби')
         collector.delete_book_from_favorites('Гордость и предубеждение и зомби')
         assert 'Гордость и предубеждение и зомби' not in collector.get_list_of_favorites_books()
 
-    def test_add_new_book_two_times_one_book(self):  # дважды одну книгу добавить нельзя
+    def test_add_new_book_two_times_one_book(self):
         collector = BooksCollector()
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
         assert len(collector.get_books_rating()) == 1
 
-    def test_get_books_rating_vocabulary(self): # Проверка метода get_books_rating.
+    def test_get_books_rating_vocabulary(self):
         collector = BooksCollector()
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
         assert collector.get_books_rating() == {'Что делать, если ваш кот хочет вас убить': 1}
 
-    def test_get_list_of_favorite_books_add_and_get(self): # получаем список Избранных книг
+    def test_get_list_of_favorite_books_add_and_get(self):
         collector = BooksCollector()
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
         collector.add_book_in_favorites('Что делать, если ваш кот хочет вас убить')
